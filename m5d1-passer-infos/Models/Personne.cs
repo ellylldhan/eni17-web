4a474ebc35a64e53fc7036b7eeeab6be1ffc832f
Galette-Saucisse <rinux.rinux@gmail.com>
2021-04-22 01:01:40 +0200

    ajout demo template wizard m5d2

---
+++ m5d1-passer-infos/Models/Personne.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace m5d1_passer_infos.Models
+{
+    public class Personne
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public int Age { get; set; }
+    }
+}
