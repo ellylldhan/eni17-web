201e55e5be1e72369d77e3434d62851ef0231bee
Galette-Saucisse <rinux.rinux@gmail.com>
2021-04-22 12:19:15 +0200

    m5tp1 chats (tp3) - fin

--- tp3-chats/Controllers/ChatController.cs
+++ tp3-chats/Controllers/ChatController.cs
@@ -3,27 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using tp3_chats.Models;
 
 namespace tp3_chats.Controllers
 {
     public class ChatController : Controller
     {
+        private static List<Chat> chats = Chat.GetMeuteDeChats();
+
         // GET: Chat
         public ActionResult Index()
         {
-            return View();
+            return View(chats);
         }
 
         // GET: Chat/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            return View(chats.FirstOrDefault(c => c.Id == id));
         }
 
         // GET: Chat/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            return View(chats.FirstOrDefault(c => c.Id == id));
         }
 
         // POST: Chat/Delete/5
@@ -32,8 +35,7 @@ namespace tp3_chats.Controllers
         {
             try
             {
-                // TODO: Add delete logic here
-
+                chats.Remove(chats.FirstOrDefault(c => c.Id == id));
                 return RedirectToAction("Index");
             }
             catch
