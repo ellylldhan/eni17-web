6aa4b03d3b19f83abb8033c3f00533869e950939
Galette-Saucisse <rinux.rinux@gmail.com>
2021-04-30 10:32:25 +0200

    :fire: changement de poste + migration aux fraises = dev en aveugle

--- m6tp1-dojo/Models/SamouraiViewModel.cs
+++ m6tp1-dojo/Models/SamouraiViewModel.cs
@@ -11,10 +11,6 @@ namespace m6tp1_dojo.Models
         public Samourai Samourai { get; set; }
         public List<Arme> Armes { get; set; }
         public List<ArtMartial> ArtsMartiaux { get; set; }
-        //public List<SelectListItem> ArtsMartiaux { get; set; }
         public List<long> ArtsMartiauxIds { get; set; }
-
-
-
     }
 }
